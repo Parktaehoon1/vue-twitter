fecea09a066978d5d80579ad15ea88fe30c59c1a
Parktaehoon1 <92parkth@gmail.com>
2022-09-26 17:13:09 +0900

    tailwind css 활용 사이드바 작업

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,105 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex h-screen">
+    <!-- side section -->
+    <div
+      class="w-1/4 pt-5 ml-10 flex flex-col justify-between border-r border-gray-200"
+    >
+      <div class="flex flex-col items-center lg:items-start">
+        <!-- twitter Logo -->
+        <i class="fa-brands fa-twitter text-3xl text-primary lg:ml-4 mb-3"></i>
+        <!-- sidemenu icons-->
+        <div class="flex flex-col items-start">
+          <div
+            class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full"
+          >
+            <i class="fas fa-home fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">홈</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full"
+          >
+            <i class="fas fa-hashtag fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">탐색하기</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full"
+          >
+            <i class="far fa-bell fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">알림</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full"
+          >
+            <i class="far fa-envelope fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">쪽지</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full"
+          >
+            <i class="far fa-bookmark fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">북마크</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full"
+          >
+            <i class="far fa-list-alt fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">리스트</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full"
+          >
+            <i class="far fa-user fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">프로필</span>
+          </div>
+          <div
+            class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full"
+          >
+            <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
+            <span class="ml-5 text-lg hidden lg:inline-block">더보기</span>
+          </div>
+        </div>
+        <!-- tweet buttton -->
+        <div class="w-full lg:pr-3 flex justify-center">
+          <button
+            class="mt-3 bg-primary text-white lg:w-full w-12 h-12 rounded-full hover:bg-dark"
+          >
+            <span class="hidden lg:block">트윗</span>
+            <i class="fas fa-plus lg:hidden"></i>
+          </button>
+        </div>
+      </div>
+      <!-- profile button -->
+      <div class="lg:pr-3 mb-3">
+        <button
+          class="hidden lg:flex mt-3 px-2 py-1 w-full h-12 rounded-full hover:bg-blue-50 items-center"
+        >
+          <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full" />
+          <div class="lg:ml-2 hidden lg:block">
+            <div class="text-sm font-bold">taehoon.com</div>
+            <div class="text-xs text-gray-500 text-left">@taehoon</div>
+          </div>
+          <i
+            class="ml-auto fas fa-ellipsis-h fa-fw text-xs hidden lg:block"
+          ></i>
+        </button>
+        <div class="lg:hidden flex justify-center">
+          <img
+            src="http://picsum.photos/100"
+            class="w-10 h-10 rounded-full cursor-pointer hover:opacity-80"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="flex-"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    return {};
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1DA1F2",
+        dark: "#1a91da",
+        light: "#a5d015",
+        lighter: "#D4E5F4",
+      },
+    },
+  },
+  plugins: [],
+};
